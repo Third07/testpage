3187a3cfe9a30e4dfeede35549d6b329862c7e5a
Third07 <thethird0716@gmail.com>
2026-06-24 01:38:32 +0800

    Update videoPost.js

--- website/js/features/videoPost.js
+++ website/js/features/videoPost.js
@@ -12,8 +12,7 @@ container.appendChild(selector.el);
 
 const card = document.createElement('div');
 card.className = 'card';
-
-card.innerHTML = 
+card.innerHTML = `
   <div class="card__title">Video URL Publisher</div>
 
   <div class="form-group">
